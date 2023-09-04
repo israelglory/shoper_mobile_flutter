f3ca63c0fd8be6070fbb2038796f904d4c00f9ad
israelglory <iglory6@gmail.com>
2023-09-04 14:26:47 +0100

    mediaQuery

--- lib/features/home/home_view.dart
+++ lib/features/home/home_view.dart
@@ -19,171 +19,178 @@ class HomeView extends StatelessWidget {
             body: SafeArea(
               child: Stack(
                 children: [
-                  Padding(
-                    padding: const EdgeInsets.only(left: 25.0, right: 25.0),
-                    child: Column(
-                      children: [
-                        const Spacing.height(36.0),
-                        Row(
+                  SizedBox(
+                    height: MediaQuery.of(context).size.height,
+                    width: MediaQuery.of(context).size.width,
+                    child: SingleChildScrollView(
+                      child: Padding(
+                        padding: const EdgeInsets.only(left: 25.0, right: 25.0),
+                        child: Column(
                           children: [
-                            ///Avatar Design
-                            const CircleAvatar(
-                              backgroundImage: NetworkImage(
-                                  'https://expertphotography.b-cdn.net/wp-content/uploads/2020/08/profile-photos-4.jpg'),
-                              radius: 26,
+                            const Spacing.height(36.0),
+                            Row(
+                              children: [
+                                ///Avatar Design
+                                const CircleAvatar(
+                                  backgroundImage: NetworkImage(
+                                      'https://expertphotography.b-cdn.net/wp-content/uploads/2020/08/profile-photos-4.jpg'),
+                                  radius: 26,
+                                ),
+                                const Spacing.width(10.0),
+                                Expanded(
+                                  child: Column(
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
+                                    children: [
+                                      AppText(
+                                        'Good morning 👋🏽',
+                                        color: Colors.black
+                                            .withOpacity(0.6000000238418579),
+                                        fontSize: 13,
+                                        fontWeight: FontWeight.w400,
+                                        height: 1,
+                                      ),
+                                      const AppText(
+                                        'Jossy Adebayomi',
+                                        color: Colors.black,
+                                        fontSize: 14,
+                                        fontWeight: FontWeight.w500,
+                                      )
+                                    ],
+                                  ),
+                                ),
+                                const Icon(Icons.shopping_cart_outlined)
+                              ],
                             ),
-                            const Spacing.width(10.0),
-                            Expanded(
-                              child: Column(
-                                crossAxisAlignment: CrossAxisAlignment.start,
+                            const Spacing.height(36.0),
+
+                            Container(
+                              padding: const EdgeInsets.symmetric(
+                                  horizontal: 16.0, vertical: 18.0),
+                              decoration: ShapeDecoration(
+                                color: const Color(0xFFF5F5F5),
+                                shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(16),
+                                ),
+                              ),
+                              child: const Row(
                                 children: [
+                                  Icon(
+                                    Icons.search,
+                                    color: Color(0xFFBDBDBD),
+                                  ),
+                                  Spacing.width(10.0),
                                   AppText(
-                                    'Good morning 👋🏽',
-                                    color: Colors.black
-                                        .withOpacity(0.6000000238418579),
+                                    'Search',
+                                    color: Color(0xFFBDBDBD),
                                     fontSize: 13,
                                     fontWeight: FontWeight.w400,
-                                    height: 1,
                                   ),
-                                  const AppText(
-                                    'Jossy Adebayomi',
-                                    color: Colors.black,
-                                    fontSize: 14,
-                                    fontWeight: FontWeight.w500,
-                                  )
                                 ],
                               ),
                             ),
-                            const Icon(Icons.shopping_cart_outlined)
-                          ],
-                        ),
-                        const Spacing.height(36.0),
+                            const Spacing.height(24.0),
 
-                        Container(
-                          padding: const EdgeInsets.symmetric(
-                              horizontal: 16.0, vertical: 18.0),
-                          decoration: ShapeDecoration(
-                            color: const Color(0xFFF5F5F5),
-                            shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(16),
+                            ///Start Shopping Design
+                            const Row(
+                              children: [
+                                AppText(
+                                  'Start shopping',
+                                  color: Colors.black,
+                                  fontSize: 14,
+                                  fontWeight: FontWeight.w600,
+                                  letterSpacing: -0.28,
+                                ),
+                                Spacer(),
+                                AppText(
+                                  'See all',
+                                  color: Colors.black,
+                                  fontSize: 14,
+                                  fontStyle: FontStyle.italic,
+                                  fontWeight: FontWeight.w500,
+                                  textDecoration: TextDecoration.underline,
+                                )
+                              ],
                             ),
-                          ),
-                          child: const Row(
-                            children: [
-                              Icon(
-                                Icons.search,
-                                color: Color(0xFFBDBDBD),
-                              ),
-                              Spacing.width(10.0),
-                              AppText(
-                                'Search',
-                                color: Color(0xFFBDBDBD),
-                                fontSize: 13,
-                                fontWeight: FontWeight.w400,
-                              ),
-                            ],
-                          ),
-                        ),
-                        const Spacing.height(24.0),
+                            const Spacing.height(24.0),
 
-                        ///Start Shopping Design
-                        const Row(
-                          children: [
-                            AppText(
-                              'Start shopping',
-                              color: Colors.black,
-                              fontSize: 14,
-                              fontWeight: FontWeight.w600,
-                              letterSpacing: -0.28,
-                            ),
-                            Spacer(),
-                            AppText(
-                              'See all',
-                              color: Colors.black,
-                              fontSize: 14,
-                              fontStyle: FontStyle.italic,
-                              fontWeight: FontWeight.w500,
-                              textDecoration: TextDecoration.underline,
-                            )
-                          ],
-                        ),
-                        const Spacing.height(24.0),
+                            ///Banner Image
+                            Image.asset(AppAssets.discount),
+                            const Spacing.height(24.0),
 
-                        ///Banner Image
-                        Image.asset(AppAssets.discount),
-                        const Spacing.height(24.0),
+                            ///Brands Design
+                            const Row(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                BrandsWidget(
+                                  brandName: 'Adidas',
+                                  image: 'assets/images/adidas.png',
+                                ),
+                                BrandsWidget(
+                                  brandName: 'Nike',
+                                  image: 'assets/images/nike.png',
+                                ),
+                                BrandsWidget(
+                                  brandName: 'Vulgar',
+                                  image: 'assets/images/vulgar.png',
+                                ),
+                                BrandsWidget(
+                                  brandName: 'H&H',
+                                  image: 'assets/images/hh.png',
+                                )
+                              ],
+                            ),
+                            const Spacing.height(24.0),
 
-                        ///Brands Design
-                        const Row(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            BrandsWidget(
-                              brandName: 'Adidas',
-                              image: 'assets/images/adidas.png',
+                            ///Brands Design
+                            const Row(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                BrandsWidget(
+                                  brandName: 'Vulgar',
+                                  image: 'assets/images/vulgar.png',
+                                ),
+                                BrandsWidget(
+                                  brandName: 'Adidas',
+                                  image: 'assets/images/adidas.png',
+                                ),
+                                BrandsWidget(
+                                  brandName: 'H&H',
+                                  image: 'assets/images/hh.png',
+                                ),
+                                BrandsWidget(
+                                  brandName: 'Nike',
+                                  image: 'assets/images/nike.png',
+                                ),
+                              ],
                             ),
-                            BrandsWidget(
-                              brandName: 'Nike',
-                              image: 'assets/images/nike.png',
-                            ),
-                            BrandsWidget(
-                              brandName: 'Vulgar',
-                              image: 'assets/images/vulgar.png',
-                            ),
-                            BrandsWidget(
-                              brandName: 'H&H',
-                              image: 'assets/images/hh.png',
-                            )
-                          ],
-                        ),
-                        const Spacing.height(24.0),
-
-                        ///Brands Design
-                        const Row(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            BrandsWidget(
-                              brandName: 'Vulgar',
-                              image: 'assets/images/vulgar.png',
-                            ),
-                            BrandsWidget(
-                              brandName: 'Adidas',
-                              image: 'assets/images/adidas.png',
-                            ),
-                            BrandsWidget(
-                              brandName: 'H&H',
-                              image: 'assets/images/hh.png',
-                            ),
-                            BrandsWidget(
-                              brandName: 'Nike',
-                              image: 'assets/images/nike.png',
+                            const Spacing.height(24.0),
+                            Row(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                Image.asset(
+                                  'assets/images/arrowBack.png',
+                                  height: 44,
+                                  width: 44,
+                                ),
+                                Image.asset(
+                                  'assets/images/dots.png',
+                                  height: 56,
+                                  width: 66,
+                                ),
+                                Image.asset(
+                                  'assets/images/arrowForward.png',
+                                  height: 44,
+                                  width: 44,
+                                ),
+                              ],
                             ),
                           ],
                         ),
-                        const Spacing.height(24.0),
-                        Row(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            Image.asset(
-                              'assets/images/arrowBack.png',
-                              height: 44,
-                              width: 44,
-                            ),
-                            Image.asset(
-                              'assets/images/dots.png',
-                              height: 56,
-                              width: 66,
-                            ),
-                            Image.asset(
-                              'assets/images/arrowForward.png',
-                              height: 44,
-                              width: 44,
-                            ),
-                          ],
-                        ),
-                      ],
+                      ),
                     ),
                   ),
 
